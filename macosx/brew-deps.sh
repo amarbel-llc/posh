7ea1f10fd0c03fd525d32a7fff9acdbf48a14011
sfriedenberg-etsy <sfriedenberg@etsy.com>
2026-06-08 17:57:52 -0400

    codemod: bring tree into treefmt compliance
    Apply `just codemod-fmt` (clang-format + shfmt) so the new lint-fmt gate
    passes. Pure formatting, no behavior change:
    
    - macosx/{brew-deps,build,osx-xcode,port-deps}.sh: shfmt (2-space indent,
      brace-on-same-line, redirect spacing).
    - src/crypto/{base64.cc,byteorder.h}, src/util/select.h: clang-format
      alignment with the repo's existing .clang-format (Mozilla-based).
    
    🤡 Generated with Clown 0.3.10+8ef9ad5
    https://github.com/amarbel-llc/clown/commit/8ef9ad59aa61af006f71841cfff721e6d150e27c

--- macosx/brew-deps.sh
+++ macosx/brew-deps.sh
@@ -15,12 +15,11 @@
 #
 # Install and/or configure the system used to provide dependencies.
 #
-install()
-{
-    # Straight from https://brew.sh
-    if ! brew --version > /dev/null 2>&1; then
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    fi
+install() {
+  # Straight from https://brew.sh
+  if ! brew --version >/dev/null 2>&1; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
 }
 
 #
@@ -29,13 +28,12 @@ install()
 # dependencies for the build system, support for things like previous
 # OS versions and fat binaries is not needed.
 #
-deps()
-{
-    brew update
-    brew update
-    brew upgrade
-    brew reinstall tmux
-    brew reinstall protobuf
+deps() {
+  brew update
+  brew update
+  brew upgrade
+  brew reinstall tmux
+  brew reinstall protobuf
 }
 
 #
@@ -47,20 +45,18 @@ deps()
 # fat library that will run on any machine.  (This takes about 15
 # minutes on current Travis infrastructure.)
 #
-package_deps()
-{
-    deps
-    brew rm protobuf
-    brew install protobuf --universal --build-bottle
+package_deps() {
+  deps
+  brew rm protobuf
+  brew install protobuf --universal --build-bottle
 }
 
 #
 # Describe the dependencies installed and used as best as possible.
 #
-describe()
-{
-    brew --version > brew-version.txt
-    brew info --json=v1 --installed > brew-info.json
+describe() {
+  brew --version >brew-version.txt
+  brew info --json=v1 --installed >brew-info.json
 }
 
 #
--- macosx/build.sh
+++ macosx/build.sh
@@ -20,17 +20,28 @@
 
 set -e
 
-protobuf_LIBS=$(l=libprotobuf.a; for i in /opt/local/lib /usr/local/lib; do  if [ -f $i/$l ]; then echo $i/$l; fi; done)
-if [ -z "$protobuf_LIBS" ]; then echo "Can't find libprotobuf.a"; exit 1; fi
+protobuf_LIBS=$(
+  l=libprotobuf.a
+  for i in /opt/local/lib /usr/local/lib; do if [ -f $i/$l ]; then echo $i/$l; fi; done
+)
+if [ -z "$protobuf_LIBS" ]; then
+  echo "Can't find libprotobuf.a"
+  exit 1
+fi
 export protobuf_LIBS
-if ! pkg-config --cflags protobuf > /dev/null 2>&1; then
-    protobuf_CFLAGS=-I$(for i in /opt /usr; do d=$i/local/include; if [ -d $d/google/protobuf ]; then echo $d; fi; done)
-    if [ "$protobuf_CFLAGS" = "-I" ]; then echo "Can't find protobuf includes"; exit 1; fi
-    export protobuf_CFLAGS
+if ! pkg-config --cflags protobuf >/dev/null 2>&1; then
+  protobuf_CFLAGS=-I$(for i in /opt /usr; do
+    d=$i/local/include
+    if [ -d $d/google/protobuf ]; then echo $d; fi
+  done)
+  if [ "$protobuf_CFLAGS" = "-I" ]; then
+    echo "Can't find protobuf includes"
+    exit 1
+  fi
+  export protobuf_CFLAGS
 fi
 
 echo "Building into prefix..."
-
 
 #
 # XXX This script abuses Configure's --prefix argument badly.  It uses
@@ -42,65 +53,62 @@ PREFIX="$(pwd)/prefix"
 HOST="x86_64-apple-macosx${MACOSX_DEPLOYMENT_TARGET}"
 ARCH_TRIPLES="x86_64-apple-macosx arm64-apple-macos"
 
-pushd .. > /dev/null
+pushd .. >/dev/null
 
-if [ ! -f configure ];
-then
-    echo "Running autogen."
-    PATH=/opt/local/bin:$PATH ./autogen.sh
+if [ ! -f configure ]; then
+  echo "Running autogen."
+  PATH=/opt/local/bin:$PATH ./autogen.sh
 fi
 
 #
 # Build archs one by one.
 #
 for triple in $ARCH_TRIPLES; do
-    arch=$(echo $triple | cut -d- -f1)
-    echo "Building for ${arch}..."
-    prefix="${PREFIX}_${arch}"
-    rm -rf "${prefix}"
-    mkdir "${prefix}"
-    if ./configure --prefix="${prefix}/local" --build="${triple}${MACOSX_DEPLOYMENT_TARGET}"\
-		   --host="${HOST}" \
-		   CC="cc -arch ${arch}" CPP="cc -arch ${arch} -E" CXX="c++ -arch ${arch}" \
-		   TINFO_LIBS=-lncurses &&
-	    make clean &&
-	    make install -j8 V=1 &&
-	    rm -f "${prefix}/etc"
-    then
-	# mosh-client built with Xcode 3.1.2 bus-errors if the binary is stripped.
-	# strip "${prefix}/local/bin/mosh-client" "${prefix}/local/bin/mosh-server"
-	BUILT_ARCHS="$BUILT_ARCHS $arch"
-    fi
+  arch=$(echo $triple | cut -d- -f1)
+  echo "Building for ${arch}..."
+  prefix="${PREFIX}_${arch}"
+  rm -rf "${prefix}"
+  mkdir "${prefix}"
+  if ./configure --prefix="${prefix}/local" --build="${triple}${MACOSX_DEPLOYMENT_TARGET}" \
+    --host="${HOST}" \
+    CC="cc -arch ${arch}" CPP="cc -arch ${arch} -E" CXX="c++ -arch ${arch}" \
+    TINFO_LIBS=-lncurses &&
+    make clean &&
+    make install -j8 V=1 &&
+    rm -f "${prefix}/etc"; then
+    # mosh-client built with Xcode 3.1.2 bus-errors if the binary is stripped.
+    # strip "${prefix}/local/bin/mosh-client" "${prefix}/local/bin/mosh-server"
+    BUILT_ARCHS="$BUILT_ARCHS $arch"
+  fi
 done
 
 if [ -z "$BUILT_ARCHS" ]; then
-    echo "No architectures built successfully"
-    exit 1
+  echo "No architectures built successfully"
+  exit 1
 fi
 
 echo "Building universal binaries for archs ${BUILT_ARCHS}..."
 
-
 rm -rf "$PREFIX"
 # Copy one architecture to get all files into place.
 for arch in $BUILT_ARCHS; do
-    cp -Rp "${PREFIX}_${arch}" "${PREFIX}"
-    break
+  cp -Rp "${PREFIX}_${arch}" "${PREFIX}"
+  break
 done
 
 # Build fat binaries
 # XXX will break with spaces in pathname
 for prog in local/bin/mosh-client local/bin/mosh-server; do
-    archprogs=()
-    for arch in $BUILT_ARCHS; do
-	archprogs+=("${PREFIX}_${arch}/$prog")
-    done
-    lipo -create "${archprogs[@]}" -output "${PREFIX}/$prog"
+  archprogs=()
+  for arch in $BUILT_ARCHS; do
+    archprogs+=("${PREFIX}_${arch}/$prog")
+  done
+  lipo -create "${archprogs[@]}" -output "${PREFIX}/$prog"
 done
 
 perl -wlpi -e 's{#!/usr/bin/env perl}{#!/usr/bin/perl}' "$PREFIX/local/bin/mosh"
 
-popd > /dev/null
+popd >/dev/null
 
 PACKAGE_VERSION=$(cat ../VERSION.stamp)
 
@@ -109,48 +117,45 @@ OUTFILE="$PACKAGE_VERSION.pkg"
 rm -f "$OUTFILE"
 
 if which -s pkgbuild; then
-    # To replace PackageMaker, you:
-    # * make a bare package with the build products
-    # * essentially take the Distribution file that PackageMaker generated and
-    #   use it as the --distribution input file for productbuild
-    echo "Preprocessing package description..."
-    PKGID=edu.mit.mosh.mosh.pkg
-    for file in Distribution; do
-	sed -e "s/@PACKAGE_VERSION@/${PACKAGE_VERSION}/g" ${file}.in > ${file}
-    done
-    echo "Running pkgbuild/productbuild..."
-    mkdir -p Resources/en.lproj
-    cp -p copying.rtf Resources/en.lproj/License
-    cp -p readme.rtf Resources/en.lproj/Readme
-    pkgbuild --root "$PREFIX" --identifier $PKGID $PKGID
-    productbuild --distribution Distribution \
-		 --resources Resources \
-		 --package-path . \
-		 "$OUTFILE"
-    echo "Cleaning up..."
-    rm -rf $PKGID
+  # To replace PackageMaker, you:
+  # * make a bare package with the build products
+  # * essentially take the Distribution file that PackageMaker generated and
+  #   use it as the --distribution input file for productbuild
+  echo "Preprocessing package description..."
+  PKGID=edu.mit.mosh.mosh.pkg
+  for file in Distribution; do
+    sed -e "s/@PACKAGE_VERSION@/${PACKAGE_VERSION}/g" ${file}.in >${file}
+  done
+  echo "Running pkgbuild/productbuild..."
+  mkdir -p Resources/en.lproj
+  cp -p copying.rtf Resources/en.lproj/License
+  cp -p readme.rtf Resources/en.lproj/Readme
+  pkgbuild --root "$PREFIX" --identifier $PKGID $PKGID
+  productbuild --distribution Distribution \
+    --resources Resources \
+    --package-path . \
+    "$OUTFILE"
+  echo "Cleaning up..."
+  rm -rf $PKGID
 else
-    echo "Preprocessing package description..."
-    INDIR=mosh-package.pmdoc.in
-    OUTDIR=mosh-package.pmdoc
-    mkdir -p "$OUTDIR"
-    pushd "$INDIR" > /dev/null
-    for file in *
-    do
-	sed -e 's/$PACKAGE_VERSION/'"$PACKAGE_VERSION"'/g' "$file" > "../$OUTDIR/$file"
-    done
-    popd > /dev/null
-    echo "Running PackageMaker..."
-    env PATH="/Applications/PackageMaker.app/Contents/MacOS:/Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS:$PATH" PackageMaker -d mosh-package.pmdoc -o "$OUTFILE" -i edu.mit.mosh.mosh.pkg
-    echo "Cleaning up..."
-    rm -rf "$OUTDIR"
+  echo "Preprocessing package description..."
+  INDIR=mosh-package.pmdoc.in
+  OUTDIR=mosh-package.pmdoc
+  mkdir -p "$OUTDIR"
+  pushd "$INDIR" >/dev/null
+  for file in *; do
+    sed -e 's/$PACKAGE_VERSION/'"$PACKAGE_VERSION"'/g' "$file" >"../$OUTDIR/$file"
+  done
+  popd >/dev/null
+  echo "Running PackageMaker..."
+  env PATH="/Applications/PackageMaker.app/Contents/MacOS:/Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS:$PATH" PackageMaker -d mosh-package.pmdoc -o "$OUTFILE" -i edu.mit.mosh.mosh.pkg
+  echo "Cleaning up..."
+  rm -rf "$OUTDIR"
 fi
 
-
-if [ -f "$OUTFILE" ];
-then
-    echo "Successfully built $OUTFILE with archs ${BUILT_ARCHS}."
+if [ -f "$OUTFILE" ]; then
+  echo "Successfully built $OUTFILE with archs ${BUILT_ARCHS}."
 else
-    echo "There was an error building $OUTFILE."
-    false
+  echo "There was an error building $OUTFILE."
+  false
 fi
