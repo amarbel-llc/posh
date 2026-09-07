982e9b9019524a8ccfb22bad53e492e139b2064c
friedenberg <1baggzpr@starbrandshoes.com>
2026-09-07 02:29:02 +0000

    bump nixpkgs-master: 567a49d → f13ff45
    conformist: 639a495 → f2660cb
    igloo: 2c8ca73 → b13d154
    nixpkgs-master: 567a49d → f13ff45
    purse-first: 61aaadb → d268546

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     igloo.inputs.nixpkgs-master.follows = "nixpkgs-master";
     utils.url = "https://flakehub.com/f/numtide/flake-utils/0.1.102";
     utils.inputs.systems.follows = "igloo/systems";
-    nixpkgs-master.url = "github:NixOS/nixpkgs/567a49d1913ce81ac6e9582e3553dd90a955875f";
+    nixpkgs-master.url = "github:NixOS/nixpkgs/f13ff45afd1bb73e640eaa08a7066dbed07e3238";
 
     # conformist — the linter+formatter multiplexer (treefmt successor, RFC
     # 0001). Supplies the runner binary, its Nix module library
